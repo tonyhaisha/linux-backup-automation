44900d814ff61ec031dc8a17edbe2c773a8435ae
tonyhaisha <mrcat5535@gmail.com>
2026-05-04 13:57:23 +0000

    backup.sh script

--- backup.sh
+++ backup.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+BACKUP_DIRS=("/home/$(whoami)/")
+REMOTE_USER="server"
+REMOTE_HOST="192.168.100.11"
+REMOTE_PORT="22"
+
+
+SSH_KEY_PATH="$HOME/.ssh/id_ed25519"
+
+LOG="var/log/scheduled_backup.log"
+
+echo "BACKUP STARTED" >> LOG
+echo $(date "+%d-%m-%Y") >> LOG
+
+sudo rsync -avz /home/ashen/ server@UbuntuBackups:/home/server/backups/$(date +"%F_%H-%M-%S")
+
+
+
+
+
+
+
+
+
+
